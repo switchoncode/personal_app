3c25dd7e0b2f1e9e7cff789f37e15e81d268de1e
khodr <khodr@switchoncode.com.au>
2021-07-24 09:21:37 +1000

    init

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Personal Expences',
+      title: 'Personal Expences.',
       theme: ThemeData(
         primarySwatch: Colors.purple,
         accentColor: Colors.amber,
